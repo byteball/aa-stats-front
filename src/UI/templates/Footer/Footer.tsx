f4988d00de06ac9c93f22163033911fe855c6777
Taump <taump@yandex.ru>
2024-06-21 15:20:48 +0400

    fix: footer

--- src/UI/templates/Footer/Footer.tsx
+++ src/UI/templates/Footer/Footer.tsx
@@ -6,24 +6,17 @@ import SocialBlock from 'UI/atoms/SocialBlock/SocialBlock';
 
 import { styles } from './styles';
 
-const Footer: FC = () => {
-  const currentData = new Date();
-  const currentYear = currentData.getFullYear();
-
-  return (
-    <Box component='footer' sx={styles.root}>
-      <SocialBlock />
-      <Box>
-        <Typography sx={styles.copyright}>
-          © 2022-{currentYear}{' '}
-          <Link href='https://obyte.org/' sx={styles.link} target='_blank'>
-            Obyte.
-          </Link>{' '}
-          All Rights Reserved
-        </Typography>
-      </Box>
+const Footer: FC = () => (
+  <Box component='footer' sx={styles.root}>
+    <SocialBlock />
+    <Box>
+      <Typography sx={styles.copyright}>
+        <Link href='https://obyte.org/' sx={styles.link} target='_blank'>
+          Built on Obyte
+        </Link>
+      </Typography>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default memo(Footer);
